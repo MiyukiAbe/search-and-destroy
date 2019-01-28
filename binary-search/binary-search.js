a307cf44fd0ea76dab67ab3702599e01ba386296
Miyuki Abe <miyuki0809@gmail.com>
2019-01-28 16:33:15 -0500

    updated

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -5,6 +5,90 @@ const binarySearch = (array, target) => {
 	
 };
 
+array = [1, 3, 5, 6, 8]
+target = 6
+
+const binarySearch(array, target, )
+
+const inTheMiddle = Math.floor (array.length / 2 ) //  5 / 2 = 2. inTheMiddle = 2
+
+array[inTheMiddle] = 5 // array[2] = 5
+if (array[inTheMiddle] > target) --> false
+if it is false, const newArr = array.slice(inTheMiddle)  // newArr = [5,6,8]
+if it is true, const newArr = array.slice(0, inTheMiddle) // newArr = [1, 3]
+
+
+newArr = [5,6,8]
+const binarySearch(newArr, target)
+
+const inTheMiddle = Math.floor (newArr.length / 2 ) //  3 / 2 = 1.5 inTheMiddle = 1
+
+newArr[inTheMiddle] = 6 // newArr[1] = 6 
+if (newArr[inTheMiddle] > target) --> false
+if it is false, const newArr = newArr.slice(inTheMiddle)  // newArr = [6,8]
+if it is true, const newArr = newArr.slice(0, inTheMiddle) // newArr = [5]
+
+newArr = [6,8]
+const binarySearch(newArr, target)
+
+const inTheMiddle = Math.floor (newArr.length / 2 ) //  2 / 2 = 1 inTheMiddle = 1
+
+newArr[inTheMiddle] = 8 // newArr[1] = 8
+if (newArr[inTheMiddle] > target) --> true
+if it is false, const newArr = newArr.slice(inTheMiddle)  // newArr = [8]
+if it is true, const newArr = newArr.slice(0, inTheMiddle) // newArr = [6]
+
+newArr = [6]
+const binarySearch(newArr, target)
+if newArr.length === 1, return newArr
+
+//recursin 
+
+array = [1, [3,5] 8]
+target = 1
+
+const binarySearch(array, target)
+
+const inTheMiddle = Math.floor (array.length / 2 ) //  3 / 2 = 1.5. inTheMiddle = 1
+
+array[inTheMiddle] = [3,5] // array[1] = [3,5]
+if (Array.isArray(array[inTheMiddle])) binarySerach (array[inTheMiddle], target) // binarySerach([3,5], 1)
+
+const inTheMiddle = Math.floor (array.length / 2 ) //  2 / 2 = 1 inTheMiddle = 1
+array[inTheMiddle] = [5] // array[1] = [5]
+
+
+if (array[inTheMiddle] > target) --> false
+if it is false, const newArr = array.slice(inTheMiddle)  // newArr = [5,6,8]
+if it is true, const newArr = array.slice(0, inTheMiddle) // newArr = [1, 3]
+
+
+newArr = [5,6,8]
+const binarySearch(newArr, target)
+
+const inTheMiddle = Math.floor (newArr.length / 2 ) //  3 / 2 = 1.5 inTheMiddle = 1
+
+newArr[inTheMiddle] = 6 // newArr[1] = 6 
+if (newArr[inTheMiddle] > target) --> false
+if it is false, const newArr = newArr.slice(inTheMiddle)  // newArr = [6,8]
+if it is true, const newArr = newArr.slice(0, inTheMiddle) // newArr = [5]
+
+newArr = [6,8]
+const binarySearch(newArr, target)
+
+const inTheMiddle = Math.floor (newArr.length / 2 ) //  2 / 2 = 1 inTheMiddle = 1
+
+newArr[inTheMiddle] = 8 // newArr[1] = 8
+if (newArr[inTheMiddle] > target) --> true
+if it is false, const newArr = newArr.slice(inTheMiddle)  // newArr = [8]
+if it is true, const newArr = newArr.slice(0, inTheMiddle) // newArr = [6]
+
+newArr = [6]
+const binarySearch(newArr, target)
+if newArr.length === 1, return newArr
+
+
+
 /*
 	EXTRA CREDIT:
 
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,43 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  
+  const currentVal = arr[0]; // 2
+  let highestNum = 4
+  let nighestNumIdx = arr.indexOf(highestNum) = 2
+  let numOfHops = highestNum //null
+  let remainingArr = arr.slice(highestNum); //
+  
+  
+  
+  
+  
+  
 };
+
+
+
+
+
+
+
+// starting point : arr[0]
+// const currentVal = arr[0]
+// const currentIndex = 0;
+
+// const numCheckList = {
+//   index : value
+// }
+
+
+// for (let i = currentIndex; i <= currentVal; i++) {
+//   when i = 0;
+//   arr[currentIndex + i] //arr[0 + 0]
+//   arr[currentIndex + i] // arr[0 + 1]
+//   arr[currentIndex + i] // arr[0 + 2]
+  
+// }
+
+
 
 module.exports = minJumps
